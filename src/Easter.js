e52a9aee4b794c6833725d67458d6df58b77d912
Stian Teien <stian.teien@gmail.com>
2022-04-13 15:46:36 +0200

    added easter

--- src/Easter.js
+++ src/Easter.js
@@ -7,11 +7,19 @@ function Easter() {
             <br />
             <h2>Påskemorro</h2>
 
-            Kanskje påskeharen kommer innom i løpet av påskeuken!
+            Velkommen til påskemorro! Hintet finner dere under haren
             <br />
         </div>
         
         <img src={process.env.PUBLIC_URL + "/easter.jpg"} width={1100} />
+        <br />
+        <div class="div_center"> 
+          <p> 
+            Bokstaven er T.
+            Nå kan dere finne ut av ordet og finne hva haren har gjemt!
+
+          </p>
+        </div>
 
       </div>
     );
